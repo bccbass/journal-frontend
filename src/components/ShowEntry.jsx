73f7a5e6c328aec348ee07291b4ef7aea77de4cb
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-08-17 10:20:48 +1000

    added api http

--- src/components/ShowEntry.jsx
+++ src/components/ShowEntry.jsx
@@ -7,7 +7,7 @@ return entry ? <>
     <h5>{entry.content}</h5>
     <p>Posted in {entry.category.name}</p>
     <button className='btn btn-danger' onClick={async () => { 
-      await fetch(`http://localhost:4001/entries/${entry._id}`, {method: 'DELETE'})
+      await fetch(`${import.meta.env.VITE_API_HOST}/${entry._id}`, {method: 'DELETE'})
       setEntries(entries.filter(targetEntry => targetEntry._id != entry._id))
       nav(`/`)
      }}>Delete Post</button>
